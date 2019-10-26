81ef5d13e473dc70a0384b73004ade0b1a9df4ab
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2019-10-26 10:39:02 -0300

    Add xpaths

--- DACScraper/spiders/coursesRetriever.py
+++ DACScraper/spiders/coursesRetriever.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+# XPATHS
+XPATH_TITLE = "//div[@class='ancora']//a/text()"
+XPATH_REQ = "//div[@class='ancora']/following-sibling::p/strong/text()"
+XPATH_CODES = "//div[@class='ancora']/following-sibling::p[contains(text(), 'OF')]//text()"
+XPATH_SYLLABUS = "//div[@class='ancora']//following-sibling::div[@class='justificado']//text()"
+
 
 class CoursesretrieverSpider(scrapy.Spider):
     name = 'coursesRetriever'
