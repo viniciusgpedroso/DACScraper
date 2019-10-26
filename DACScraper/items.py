39405193aa48a869f3fb7185d3efa36269de8843
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2019-10-26 10:38:33 -0300

    Add items fields

--- DACScraper/items.py
+++ DACScraper/items.py
@@ -11,4 +11,7 @@ import scrapy
 class DacscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    requirement = scrapy.Field()
+    syllabus = scrapy.Field()
+    codes = scrapy.Field()
