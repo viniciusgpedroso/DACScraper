78e328d50da40ce36b4b46051b4ca6f0af1f9aaa
viniciusgpedroso <vpedroso.ee@gmail.com>
2019-12-03 14:14:31 -0300

    Fix errors about group invalid int

--- DACScraper/pipelines.py
+++ DACScraper/pipelines.py
@@ -24,27 +24,52 @@ class MySQLPipeline(object):
             # Add to table materia
             add_curso = (" "
                "INSERT INTO materia (idmateria, year, title, codes, syllabus) "
-               "VALUES (%s, %s, %s, %s, %s)")
+               "VALUES (%s, %s, %s, %s, %s) "
+               "ON DUPLICATE KEY UPDATE idmateria=VALUES(idmateria), year=VALUES(year), "
+               "title=VALUES(title), codes=VALUES(codes), syllabus=VALUES(syllabus)")
             data_curso = (item['id'], item['year'], item['title'], item['codes'], item['syllabus'])
             cursor.execute(add_curso, data_curso)
+            self.cnx.commit()
             # Add to table reqs
-            add_reqs = ("INSET INTO requirements "
+            add_reqs = ("INSERT INTO requirements "
                "(grupo, idmateria, req) "
-               "VALUES (%s, %s, %s)")
+               "VALUES (%s, %s, %s) "
+               "ON DUPLICATE KEY UPDATE grupo=VALUES(grupo), idmateria=VALUES(idmateria), req=VALUES(req)")
             reqs = item['requirement']
             if reqs:
                 group = 1
                 for req in reqs:
                     for materia in req:
-                        data_reqs = (group, materia, item['id'])
+                        data_reqs = (group, item['id'], materia)
                         cursor.execute(add_reqs, data_reqs)
                     group += 1
             else:
-                data_reqs = (item['id'], None, None)
+                data_reqs = (item['id'], 0, None)
                 cursor.execute(add_reqs, data_reqs)
             self.cnx.commit()
             cursor.close()
         if isinstance(item, SemestersItem):
+            cursor = self.cnx.cursor()
+            # Add to table curso
+            add_curso = (" "
+               "INSERT INTO curso (idcurso, code, name, year) "
+               "VALUES (%s, %s, %s, %s) "
+               "ON DUPLICATE KEY UPDATE idcurso=VALUES(idcurso), code=VALUES(code), "
+               "name=VALUES(name), year=VALUES(year)")
+            data_curso = (item['id'], item['code'], item['name'], item['year'])
+            cursor.execute(add_curso, data_curso)
+            self.cnx.commit()
+            # Add to table semestres
+            add_curso = (" "
+               "INSERT INTO semestres (ind_semestre, materia, idcurso) "
+               "VALUES (%s, %s, %s) "
+               "ON DUPLICATE KEY UPDATE ind_semestre=VALUES(ind_semestre), materia=VALUES(materia), idcurso=VALUES(idcurso)")
+            for s in item['semesters']:
+                for m in item['semesters'][s]['materias']:
+                    data_curso = (int(s), m, item['id'])
+                    cursor.execute(add_curso, data_curso)
+                    self.cnx.commit()
+            cursor.close()
             logging.warning("SemesterItem not implemented in MySQLPipeline")
 
     def close_spider(self, spider):
--- DACScraper/settings.py
+++ DACScraper/settings.py
@@ -83,11 +83,11 @@ ITEM_PIPELINES = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 3600*24
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 # Decode latin characters
 FEED_EXPORT_ENCODING = 'utf-8'
